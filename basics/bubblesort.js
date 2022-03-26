ed14290d7e8021772d8bb3aad16778ba9a2210b3
Jason Walworth <jrwalworth@gmail.com>
2022-03-26 13:07:09 -0700

    bubble sort basics.

---
+++ basics/bubblesort.js
@@ -0,0 +1,24 @@
+//problem statement: sort array using bubble sort
+
+
+// efine function that accepts array
+//loop through counter that's initialized at array.length
+    //iterate through array
+        //compare current value and next value 
+        // if current is greather than next, swap
+        //else continue
+    //Decrement our counter variable
+//return sorted array
+
+function bubbleSort(arr){
+    
+    let counter = arr.length;
+    while (counter > 0) {
+        console.log(counter);
+        counter --
+    }
+    
+    
+    
+    return arr
+}
